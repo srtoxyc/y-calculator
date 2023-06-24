fc757d2e6ee30a9eac9b214690557474a2933b8a
srtoxyc <ivi1000vm@gmail.com>
2023-06-24 17:25:38 +0200

    Keys message box introduced

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -4,6 +4,8 @@ import { compile, evaluate } from 'mathjs'
 
 import Botonera from './Botonera.jsx'
 import Screen from './Screen.jsx'
+import MessageBox from './MessageBox.jsx'
+
 import '../styles/Calculadora.css'
 
 function Calculadora () {
@@ -14,13 +16,15 @@ function Calculadora () {
     const MODULUS_CHAR       = '%';
     const SQRT_CHAR          = 'sqrt';
     const FPOINT_CHAR        = '.';
+    const POW_CHAR           = '^';
     const RESET_CHAR         = 'C';
     const CALCULATE_CHAR     = '=';
 
-    const DELETE_KEY         = "Backspace"
-    const ENTER_KEY          = "Enter"
-    const SQRT_KEY           = "º"
+    const DELETE_KEY         = "Backspace";
+    const ENTER_KEY          = "Enter";
+    const SQRT_KEY           = "º";
     const RESET_KEY          = "c";
+    const POW_KEY            = "Dead";
 
     const [data, setData]    = useState('');
 
@@ -38,6 +42,10 @@ function Calculadora () {
                 setData((prevData) => prevData + SQRT_CHAR + "(");
             } else if(event.key === RESET_KEY) {
                 setData("");
+            } else if(event.key === POW_KEY) {
+                setData((prevData) => prevData + POW_CHAR);
+            } else {
+                console.log(event.key);
             }
         };
     
@@ -80,6 +88,8 @@ function Calculadora () {
                 <Screen className="screen" data={data}/>
                 <Botonera className="botonera" callback={setElement} />
             </div>
+
+            <MessageBox />
         </>
 	)
 }
